3629ab3e5185edfb0015f466a35e51e1ba3e6d79
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-09-08 12:22:19 +0900

    Update use_subprocess_exec_cmd.py

--- 8.使用subprocess，输出shell命令的正常和异常结果/use_subprocess_exec_cmd.py
+++ 8.使用subprocess，输出shell命令的正常和异常结果/use_subprocess_exec_cmd.py
@@ -17,3 +17,20 @@ print("Error:", error)
 在上面的示例中，我们使用`subprocess.run()`函数执行Shell命令，并使用`capture_output=True`参数来捕获命令的输出结果。`text=True`参数用于将输出结果以文本形式返回。`stdout`属性包含命令的标准输出，`stderr`属性包含命令的标准错误输出。
 
 您可以根据需要调整`command`变量的值来执行不同的Shell命令。然后，可以通过`result.stdout`和`result.stderr`来获取输出结果。最后，您可以使用`print()`函数打印输出结果。
+
+
+import subprocess
+def use_subprocess_exec_cmd(command):
+
+
+
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    
+    # 获取Shell返回结果
+    output = result.stdout
+    error = result.stderr
+    
+    return output,error
+    
+    
+    
