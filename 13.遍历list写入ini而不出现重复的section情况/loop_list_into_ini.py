640141a95aa4e44fbcb6b56eae3bf4c1c747d3fd
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-09-15 13:19:51 +0900

    Update loop_list_into_ini.py

--- 13.遍历list写入ini而不出现重复的section情况/loop_list_into_ini.py
+++ 13.遍历list写入ini而不出现重复的section情况/loop_list_into_ini.py
@@ -13,6 +13,8 @@ for section in sections:
 
 with open('example.ini', 'w') as f:
     config.write(f)
+
+一定要把with语句放到for循环之外，一次性写入才能解决写入的问题
 在上述代码中，首先创建了一个 ConfigParser 对象。然后，使用 add_section() 方法将列表中的元素作为节名添加到 INI 文件中。接下来，使用 set() 方法设置键和值。最后，使用 write() 方法将配置写入 INI 文件。
 
 这样，你就可以将列表中的元素作为节名遍历写入 INI 文件了。如果你想要添加更多的键和值，只需要在循环中使用 set() 方法即可。
